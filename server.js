6087fe648176924d0a0c4809e62c7a3407b71eba
Paulo Cesar - Amor e Patas <skedarmusic@gmail.com>
2026-06-15 00:30:28 -0300

    chore: remover restricao de hostname no server.js para permitir escuta em 0.0.0.0

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
 const port = process.env.PORT || 3000;
 
-const app = next({ dev, hostname, port });
+const app = next({ dev, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -25,6 +24,6 @@ app.prepare().then(() => {
       process.exit(1);
     })
     .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
+      console.log(`> Ready on port ${port}`);
     });
 });
